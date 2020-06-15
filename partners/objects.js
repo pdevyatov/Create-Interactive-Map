c9809e94c524aae7be1777d0e397f87fa22b22eb
pdevyatov <p.devyatov@gmail.com>
2020-06-15 18:26:52 +0300

    First commit

---
+++ partners/objects.js
@@ -0,0 +1,126 @@
+﻿let markers = {
+    objects: [
+        {
+            id: 1,
+            name: 'Espresso Bar Harbour',
+            new: true,
+            synonyms: 'cafe, espresso, harbour',
+            image: '/partners/1.jpg',
+            adress: '1656 Barrington St',
+            x: 44.646582,
+            y: -63.573827,
+            link: '',
+            percent: '10%',
+            text: `
+                Espresso Bar Harbour is small espresso bar at 1656 Barrington St. In Downtown Halifax.<br /><br />
+
+                10% discount is valid only for employees of our company. Say the code word before payment to get a discount.
+                `
+        },
+        {
+            id: 2,
+            name: 'Restaraunt The Goose',
+            new: true,
+            synonyms: 'restaraunt, the goose',
+            image: '/partners/2.jpg',
+            adress: '2053 Gottingen St',
+            x: 44.651500,
+            y: -63.582220,
+            link: '',
+            percent: '15%',
+            text: `
+                15% discount is valid only for employees of our company. Say the code word before payment to get a discount.
+                `
+        },
+        {
+            id: 3,
+            name: 'Restaraunt Eleven',
+            new: false,
+            synonyms: 'restaraunt, eleven',
+            image: '/partners/3.jpg',
+            adress: '40 Alderney Dr #305',
+            x: 44.664443,
+            y: -63.570204,
+            link: '',
+            percent: '5%',
+            text: `
+                5% discount is valid only for employees of our company. Say the code word before payment to get a discount.
+                `
+        },
+        {
+            id: 4,
+            name: 'Flowers Shop Vetka',
+            new: false,
+            synonyms: 'flowers, vetka',
+            image: '/partners/10.jpg',
+            adress: '69 Portland St',
+            x: 44.665720,
+            y: -63.566949,
+            link: '',
+            percent: '30%',
+            text: `
+                30% discount is valid only for employees of our company. Say the code word before payment to get a discount.
+                `
+        },
+        {
+            id: 5,
+            name: 'GYM PowerGlobe',
+            new: false,
+            synonyms: 'sport, gym, powerglobe',
+            image: '/partners/9.jpg',
+            adress: '7071 Bayers Rd',
+            x: 44.656151,
+            y: -63.625857,
+            link: '',
+            percent: '25%',
+            text: `
+                25% discount is valid only for employees of our company. Say the code word before payment to get a discount.
+                `
+        },
+        {
+            id: 6,
+            name: 'Medical Center 103',
+            new: false,
+            synonyms: 'health, medical',
+            image: '/partners/8.jpg',
+            adress: '302 Windmill Rd',
+            x: 44.677935, 
+            y: -63.591268,
+            link: '',
+            percent: '5%',
+            text: `
+                5% discount is valid only for employees of our company. Say the code word before payment to get a discount.
+                `
+        },
+        {
+            id: 7,
+            name: 'Hotel SomeHotel',
+            new: false,
+            synonyms: 'hotel',
+            image: null,
+            adress: '250 Saint Margarets Bay Road',
+            x: 44.639766,
+            y: -63.624240,
+            percent: '15%',
+            text: `
+                15% discount is valid only for employees of our company. Say the code word before payment to get a discount.
+                `
+        },
+        {
+            id: 8,
+            name: 'Beauty saloon on Primrose, 6',
+            new: false,
+            synonyms: 'beauty',
+            image: null,
+            adress: '6 Primrose St',
+            x: 44.681546,
+            y: -63.586126,
+            link: '',
+            percent: '5%',
+            text: `
+                5% discount is valid only for employees of our company. Say the code word before payment to get a discount.
+                `
+        }
+    ]
+
+}
